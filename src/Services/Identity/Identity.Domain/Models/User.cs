be33667b00132f463579e72517ae52ebd782f2af
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-24 14:29:33 +0300

    Add shared enum

--- src/Services/Identity/Identity.Domain/Models/User.cs
+++ src/Services/Identity/Identity.Domain/Models/User.cs
@@ -1,5 +1,5 @@
-﻿using Identity.Domain.Enums;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using Shared.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
