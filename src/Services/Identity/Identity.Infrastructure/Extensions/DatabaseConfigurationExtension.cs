a66960b2063a0f0aa7e43006ee943c3908967ec4
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-24 14:30:17 +0300

    Add auto runtime migrations apply

---
+++ src/Services/Identity/Identity.Infrastructure/Extensions/DatabaseConfigurationExtension.cs
@@ -0,0 +1,24 @@
+﻿using Identity.Infrastructure.Persistance.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Extensions
+{
+    public static class DatabaseConfigurationExtension
+    {
+        public static async Task ApplyMigrations(this IApplicationBuilder app)
+        {
+            using var services = app.ApplicationServices.CreateScope();
+
+            var dbContext = services.ServiceProvider.GetService<IdentityContext>();
+
+            await dbContext?.Database.MigrateAsync();
+        }
+    }
+}
