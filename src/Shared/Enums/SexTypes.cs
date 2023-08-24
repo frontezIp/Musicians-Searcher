9c19fa70ebaf0a6cb379ef748fcd2faa3549f8db
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-24 14:27:07 +0300

    Add sex types enum that is common to several microservices. Remove auto apply migrations during runtime.

--- src/Shared/Enums/SexTypes.cs
+++ src/Shared/Enums/SexTypes.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Identity.Domain.Enums
+namespace Shared.Enums
 {
     public enum SexTypes
     {
