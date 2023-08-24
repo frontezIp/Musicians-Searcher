627897ad28f9d760da51fdfe1addcbb53f7f158e
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-24 14:30:59 +0300

    Add musicians service initial project setup

---
+++ src/Services/Musicians/Musicians.Domain/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musicians.Domain.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class Genre
+    {
+        [BsonId, BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+
+        [BsonElement("name"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string Name { get; set; } = null!;
+    }
+}
