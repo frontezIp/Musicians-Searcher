8e45a8ff5a6d732668d5ba5c36fedede85d9de6f
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 17:36:52 +0300

    Add response dtos

--- src/Services/Identity/Identity.Application/DTOs/ResponseDTOs/UserResponseDto.cs
+++ src/Services/Identity/Identity.Application/DTOs/ResponseDTOs/UserResponseDto.cs
@@ -1,11 +1,4 @@
-﻿using Shared.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Identity.Application.DTOs.ResponseDTOs
+﻿namespace Identity.Application.DTOs.ResponseDTOs
 {
     public class UserResponseDto
     {
