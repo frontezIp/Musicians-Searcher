c8787d8752915c955ff99269f3148b0aef3576e7
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 17:11:00 +0300

    Add response dtos

---
+++ src/Services/Identity/Identity.Application/DTOs/ResponseDTOs/UserResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.DTOs.ResponseDTOs
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }    
+        public string Username { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? SecondName { get; set; } = string.Empty;
+        public string? Photo { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Biography { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string SexType { get; set; } = string.Empty;
+    }
+}
