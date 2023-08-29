6c3aa4a9d85fc828194a91c4f607fa9c6260a1b8
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 16:52:43 +0300

    Add cors policy and versioning.

--- src/Services/Identity/Identity.API/Extensions/DependencyInjection.cs
+++ src/Services/Identity/Identity.API/Extensions/DependencyInjection.cs
@@ -1,10 +1,43 @@
-﻿namespace Identity.API
+﻿using Identity.API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions;
+
+namespace Identity.API
 {
     public static class DependencyInjection
     {
-        public static void ConfigureAPI(this IServiceCollection services) 
+        public static void ConfigureAPI(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddControllers();
+            services.ConfigureVersioning();
+            services.ConfigureCors();
+            services.ConfigureSwagger(configuration);
+            services.ConfigureIdentity(configuration);
         }
+
+        private static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        }
+
+
+        private static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
+
+
     }
 }
