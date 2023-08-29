e7d9544db478c35269aa816d65ffeba194e83125
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 17:10:52 +0300

    Add user request dto

---
+++ src/Services/Identity/Identity.Application/DTOs/RequestDTOs/UserRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Domain.Models;
+using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.DTOs.RequestDTOs
+{
+    public class UserRequestDto
+    {
+        public string? Username { get; set; }    
+        public string Password { get; set; } = string.Empty;
+        public string? Name { get; set; }
+        public string? SecondName { get; set; }
+        public string? Photo { get; set; }
+        public Guid CityId { get; set; }
+        public int Age { get; set; }
+        public string? Biography { get; set; }
+        public SexTypes SexTypeId { get; set; }
+    }
+}
