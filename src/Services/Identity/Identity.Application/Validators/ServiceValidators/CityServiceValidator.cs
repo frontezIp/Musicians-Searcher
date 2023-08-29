a2e4f8b28fbdde64fb95e275db1560696653167a
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 17:32:02 +0300

    Add service validators.

---
+++ src/Services/Identity/Identity.Application/Validators/ServiceValidators/CityServiceValidator.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Application.Interfaces.Persistance;
+using Identity.Application.Interfaces.ServiceValidators;
+using Identity.Domain.Exceptions.NotFoundException;
+using Identity.Domain.Models;
+
+namespace Identity.Application.Validators.ServiceValidators
+{
+    internal class CityServiceValidator : ICityServiceValidator
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public CityServiceValidator(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        public async Task<City> CheckIfCityExistsAndGetAsync(Guid cityId, bool trackChanges)
+        {
+            var city = await _cityRepository.GetCityById(cityId, trackChanges);
+            if (city == null)
+                throw new CityNotFoundException();
+            return city;
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.Application/Validators/ServiceValidators/UserServiceValidator.cs
@@ -0,0 +1,27 @@
+﻿using Identity.Application.Interfaces.ServiceValidators;
+using Identity.Domain.Exceptions.NotFoundException;
+using Identity.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Application.Validators.ServiceValidators
+{
+    internal class UserServiceValidator : IUserServiceValidator
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserServiceValidator(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<User> CheckIfUserExistsAndGetByIdAsync(Guid userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            if (user == null)
+            {
+                throw new UserNotFoundException();
+            }
+            return user;
+        }
+    }
+}
