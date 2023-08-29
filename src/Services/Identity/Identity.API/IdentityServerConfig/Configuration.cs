fb16107d82068cf792ae53ddeb1cff1707daca7d
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 16:55:28 +0300

    Add identity server config.

---
+++ src/Services/Identity/Identity.API/IdentityServerConfig/Configuration.cs
@@ -0,0 +1,45 @@
+﻿using Identity.Domain.IdentityConstants;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Diagnostics;
+
+namespace Identity.API.IdentityServerConfig
+{
+    public static class Configuration
+    {
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource> {
+                new ApiResource(ApiResourcesConstants.MUSICIANS_API_RESOURCE_NAME),
+                new ApiResource(ApiResourcesConstants.CHAT_API_RESOURCE_NAME)
+            };
+
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client> 
+            {
+                new Client
+                {
+                    ClientId = SwaggerClientConstants.CLIENT_ID,
+                    ClientSecrets = { new Secret(SwaggerClientConstants.CLIENT_SECRET.ToSha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    RequireConsent = false,
+                    AllowedScopes = 
+                    {
+                        ApiResourcesConstants.CHAT_API_RESOURCE_NAME,
+                        IdentityServerConstants.LocalApi.ScopeName,
+                        ApiResourcesConstants.MUSICIANS_API_RESOURCE_NAME,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+    }
+}
