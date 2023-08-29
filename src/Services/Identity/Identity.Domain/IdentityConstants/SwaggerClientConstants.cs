3593888358bef3cca00948991ff41339a534988a
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 17:33:30 +0300

    Add identity constants

---
+++ src/Services/Identity/Identity.Domain/IdentityConstants/SwaggerClientConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Domain.IdentityConstants
+{
+    public static class SwaggerClientConstants
+    {
+        public const string CLIENT_ID = "ClientId";
+        public const string CLIENT_SECRET = "ClientSecret";
+    }
+}
