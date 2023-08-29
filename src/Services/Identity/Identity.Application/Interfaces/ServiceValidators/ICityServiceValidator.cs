7975d6ac4f8b4164fe19af0d234ff7561bd0f1c0
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-08-29 17:30:52 +0300

    Add services validators interfaces

---
+++ src/Services/Identity/Identity.Application/Interfaces/ServiceValidators/ICityServiceValidator.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Interfaces.ServiceValidators
+{
+    public interface ICityServiceValidator
+    {
+        Task<City> CheckIfCityExistsAndGetAsync(Guid cityId, bool trackChanges);
+    }
+}
---
+++ src/Services/Identity/Identity.Application/Interfaces/ServiceValidators/IUserServiceValidator.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Interfaces.ServiceValidators
+{
+    public interface IUserServiceValidator
+    {
+        Task<User> CheckIfUserExistsAndGetByIdAsync(Guid userId);
+    }
+}
