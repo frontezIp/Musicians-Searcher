758e11820266450a2a4b2d1d7705d2320ffa0efd
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:34:12 +0300

    Add options classes

---
+++ src/Services/Musicians/Musicians.Infrastructure/Persistance/Options/CollectionsNamesOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Musicians.Infrastructure.Persistance.Options
+{
+    public class CollectionsNamesOptions
+    {
+        public string MusiciansCollectionName { get; set; } = string.Empty;
+
+        public string GenresCollectionName { get; set;} = string.Empty;
+
+        public string SkillsCollectionName { get; set;}= string.Empty;
+    }
+}
