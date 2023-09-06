00b01f6ef4f57ebbdbbc12b181ea1dfdccdef47b
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:33:36 +0300

    Add constants

---
+++ src/Services/Musicians/Musicians.Domain/Constants/RequestParametersConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Musicians.Domain.Constants
+{
+    public static class RequestParametersConstants
+    {
+        public const int DEFAULT_MAX_PAGE_SIZE = 20;
+
+        public const int DEFAULT_PAGE_NUMBER = 1;
+
+        public const int DEFAULT_PAGE_SIZE = 10;
+    }
+}
