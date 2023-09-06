a2af152650d2fe47712c09b3f074990e3c4eedc3
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:34:56 +0300

    Add request validators

---
+++ src/Services/Musicians/Musicians.Application/Validators/RequestValidators/GetFilteredMusiciansRequestDtoValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Musicians.Application.DTOs.RequestDTOs;
+
+namespace Musicians.Application.Validators.RequestValidators
+{
+    internal class GetFilteredMusiciansRequestDtoValidator : AbstractValidator<GetFilteredMusiciansRequestDto>
+    {
+        public GetFilteredMusiciansRequestDtoValidator()
+        {
+            Include(new RequestParametersDtoValidator());
+
+            RuleFor(request => request.SearchTerm)
+                .MaximumLength(50)
+                .WithMessage("Search term must not be more than 50 symbols");
+
+            RuleFor(request => request.MinAge)
+                .LessThanOrEqualTo(150)
+                .WithMessage("Min age must be less or equal to 150")
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Min age must be greater than 0");
+
+            RuleFor(request => request.MaxAge)
+                .LessThanOrEqualTo(150)
+                .WithMessage("Max age must be less or equal to 150")
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Max age must be greater than 0");
+
+            When(musician => musician.MinAge > 1 && musician.MaxAge < 150, () =>
+            {
+                RuleFor(musician => musician.MinAge)
+                    .Must((model, field) => IsMinAgeLowerThanMax(field, model.MaxAge))
+                    .WithMessage("Min age must be lower than max age paramater");
+            });
+        }
+
+        private bool IsMinAgeLowerThanMax(int minAge, int maxAge)
+        {
+            return minAge <= maxAge;
+        }
+    }
+}
---
+++ src/Services/Musicians/Musicians.Application/Validators/RequestValidators/UpdateMusicianProfileRequestDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Musicians.Application.DTOs.RequestDTOs;
+
+namespace Musicians.Application.Validators.RequestValidators
+{
+    public class UpdateMusicianProfileRequestDtoValidator : AbstractValidator<UpdateMusicianProfileRequestDto>
+    {
+        public UpdateMusicianProfileRequestDtoValidator() 
+        {
+            RuleFor(musician => musician.Goal)
+                .MaximumLength(150)
+                .WithMessage("Goal must be 150 symbols or fewer");
+
+            RuleFor(musician => musician.SkillsIds)
+                .Must(skills => skills.Count < 6)
+                .WithMessage("The maximum number of skills per profile is 5");
+
+            RuleFor(musician => musician.FavouriteGenresIds)
+                .Must(genres => genres.Count <= 8)
+                .WithMessage("The maximum number of favourite genres per profile  is 8");
+        }
+    }
+}
