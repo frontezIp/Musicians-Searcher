b1fa806e00b9e1084d8b6297cfc754c4d2a78c5e
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:36:25 +0300

    Add mediator behaviors

---
+++ src/Services/Musicians/Musicians.Application/MediatoR/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Musicians.Application.MediatoR.Behaviours
+{
+    public class LoggingBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Handling {RequestName} request", typeof(TRequest).Name);
+            var response = await next();
+            _logger.LogInformation("Handled {ResponseName} response", typeof(TResponse).Name);
+
+            return response;
+        }
+    }
+}
