d0547814b87275470a92464825b76d20a8f228cb
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:32:59 +0300

    Add request features

---
+++ src/Services/Musicians/Musicians.Domain/RequestFeatures/PagedList.cs
@@ -0,0 +1,20 @@
+﻿namespace Musicians.Domain.RequestFeatures
+{
+    public class PagedList<T> : List<T>
+    {
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, long count, int pageNumber, int pageSize)
+        {
+            MetaData = new MetaData
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (long)Math.Ceiling(count / (double)pageSize)
+            };
+
+            AddRange(items);
+        }
+    }
+}
