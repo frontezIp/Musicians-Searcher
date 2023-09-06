2939912b4df72e6a30d7503cc9af0c3761eda77b
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:40:39 +0300

    Add api controllers, remove autogenerated setup classes

---
+++ src/Services/Musicians/Musicians.API/Controllers/v1/FriendsController.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Musicians.Application.MediatoR.Features.Friends.Commands.AddFriend;
+using Musicians.Application.MediatoR.Features.Friends.Commands.DeleteFriend;
+using Musicians.Application.MediatoR.Features.Friends.Queries.GetAllFriendsByGivenMusicianId;
+
+namespace Musicians.API.Controllers.v1
+{
+    [Route("api/{v:apiversion}/Musicians/{musicianId}/[controller]")]
+    public class FriendsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FriendsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllFriendsAsync(Guid musicianId, CancellationToken cancellationToken)
+        {
+            var query = new GetAllFriendsByGivenMusicianIdQuery(musicianId);
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<IActionResult> AddToFriendAsync(Guid musicianId, Guid musicianToAddId)
+        {
+            var query = new AddFriendToMusicianCommand(musicianId, musicianToAddId);
+
+            await _mediator.Send(query);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<IActionResult> DeleteFromFriendsAsync(Guid musicianId, Guid musicianToDeleteFromFriendsId)
+        {
+            var query = new DeleteFriendCommand(musicianId, musicianToDeleteFromFriendsId);
+
+            await _mediator.Send(query);
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/Services/Musicians/Musicians.API/Controllers/v1/MusiciansController.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Musicians.Application.DTOs.RequestDTOs;
+using Musicians.Application.MediatoR.Features.Musicians.Commands.UpdateMusicianProfile;
+using Musicians.Application.MediatoR.Features.Musicians.Queries.GetByID;
+using Musicians.Application.MediatoR.Features.Musicians.Queries.GetFilteredMusicians;
+using System.Text.Json;
+
+namespace Musicians.API.Controllers.v1
+{
+    public class MusiciansController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+
+        public MusiciansController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{musicianId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetMusicianByIdAsync(Guid musicianId, CancellationToken cancellationToken)
+        {
+            var query = new GetMusicianByIdQuery(musicianId);
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetFilteredMusicians([FromQuery]GetFilteredMusiciansRequestDto request, CancellationToken cancellationToken)
+        {
+            var query = new GetFilteredMusiciansQuery(request);
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            Response.Headers.Add("X-Pagination",
+                JsonSerializer.Serialize(result.metaData));
+
+            return Ok(result.musicians);
+        }
+
+        [HttpPut("{musicianId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> CurrentMusicianProfile(Guid musicianId, UpdateMusicianProfileRequestDto updateMusicianProfileRequestDto, CancellationToken cancellationToken)
+        {
+            var query = new UpdateMusicianProfileCommand(updateMusicianProfileRequestDto, musicianId);
+
+            await _mediator.Send(query, cancellationToken);
+
+            return NoContent();
+        }
+    }
+}
