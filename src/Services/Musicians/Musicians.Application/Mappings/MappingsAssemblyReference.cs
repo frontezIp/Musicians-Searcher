a9d21913fbc7937e91068e4c50e81e39e48992e4
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:37:21 +0300

    Add application assembly reference

---
+++ src/Services/Musicians/Musicians.Application/Mappings/MappingsAssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Musicians.Application.Mappings
+{
+    public class MappingsAssemblyReference
+    {
+        public static Assembly Assembly
+            => typeof(MappingsAssemblyReference).Assembly;
+    }
+}
