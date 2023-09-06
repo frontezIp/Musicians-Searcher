46a209bb7dbde1af32e2d6fcffbfd44057da3d9c
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-06 20:35:04 +0300

    Add service helpers

---
+++ src/Services/Musicians/Musicians.Application/ServiceHelpers/GenreServiceHelper.cs
@@ -0,0 +1,35 @@
+﻿using Musicians.Application.Interfaces.Persistance;
+using Musicians.Domain.Models;
+using Musicians.Domain.Exceptions.BadRequestException;
+using Musicians.Application.Interfaces.ServiceHelpers;
+
+namespace Musicians.Application.ServiceHelpers
+{
+    internal class GenreServiceHelper : IGenreServiceHelper
+    {
+        private readonly IGenresRepository _genreRepository;
+
+        public GenreServiceHelper(IGenresRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        public async Task<IEnumerable<Genre>> CheckIfGenresByGivenIdsExistsAndGet(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        {
+            if (ids is null)
+                throw new IdsParameterBadRequestException(nameof(Genre));
+
+            var genres = await _genreRepository.GetByIdsAsync(ids, cancellationToken);
+
+            if (genres.Count() != ids.Count())
+                throw new CollectionByIdsBadRequestException(nameof(Genre));
+
+            return genres;
+        }
+
+        public async Task CheckIfGinresByGivenIdsExists(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        {
+            await CheckIfGenresByGivenIdsExistsAndGet(ids, cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Musicians/Musicians.Application/ServiceHelpers/SkillServiceHelper.cs
@@ -0,0 +1,35 @@
+﻿using Musicians.Application.Interfaces.Persistance;
+using Musicians.Application.Interfaces.ServiceHelpers;
+using Musicians.Domain.Exceptions.BadRequestException;
+using Musicians.Domain.Models;
+
+namespace Musicians.Application.ServiceHelpers
+{
+    internal class SkillServiceHelper : ISkillServiceHelper
+    {
+        private readonly ISkillsRepository _skillsRepository;
+
+        public SkillServiceHelper(ISkillsRepository skillsRepository)
+        {
+            _skillsRepository = skillsRepository;
+        }
+
+        public async Task<IEnumerable<Skill>> CheckIfSkillsByGivenIdsExistsAndGet(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        {
+            if (ids is null)
+                throw new IdsParameterBadRequestException(nameof(Skill));
+
+            var skills = await _skillsRepository.GetByIdsAsync(ids, cancellationToken);
+
+            if (skills.Count() != ids.Count())
+                throw new CollectionByIdsBadRequestException(nameof(Skill));
+
+            return skills;
+        }
+
+        public async Task CheckSkillsByGivenIdsExists(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        {
+            await CheckIfSkillsByGivenIdsExistsAndGet(ids, cancellationToken);
+        }
+    }
+}
