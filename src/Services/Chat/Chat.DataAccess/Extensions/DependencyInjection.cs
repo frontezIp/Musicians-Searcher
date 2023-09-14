f9c531f1cf87f5ac571ff16f4990536735dc0828
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-14 19:32:37 +0300

    Add repositories injection

--- src/Services/Chat/Chat.DataAccess/Extensions/DependencyInjection.cs
+++ src/Services/Chat/Chat.DataAccess/Extensions/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using Chat.DataAccess.Contexts;
+using Chat.DataAccess.Repositories.Implementations;
+using Chat.DataAccess.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,12 +12,22 @@ namespace Chat.DataAccess.Extensions
         public static void ConfigureDAL(this IServiceCollection services, IConfiguration configuration)
         {
             services.ConfigureSqlContext(configuration);
+            services.ConfigureRepositories();
         }
 
         private static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ChatContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+        }
+
+        private static void ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IChatParticipantRepository, ChatParticipantRepository>();
+            services.AddScoped<IChatRoleRepository, ChatRoleRepository>();
+            services.AddScoped<IChatRoomRepository, ChatRoomRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IMessengerUserRepository, MessengerUserRepository>();
         }
     }
 }
