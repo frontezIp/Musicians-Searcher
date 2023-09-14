f50b1d234e093c0ea9ed0a70b7b66b06df7a4f5d
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-14 19:33:54 +0300

    Add paging constants

---
+++ src/Services/Chat/Chat.DataAccess/Constants/RequestParametersConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Chat.DataAccess.Constants
+{
+    public static class RequestParametersConstants
+    {
+        public const int DEFAULT_MAX_PAGE_SIZE = 20;
+
+        public const int DEFAULT_PAGE_NUMBER = 1;
+
+        public const int DEFAULT_PAGE_SIZE = 10;
+    }
+}
