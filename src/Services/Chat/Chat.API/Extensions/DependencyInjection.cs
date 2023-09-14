63038bd591be9f604e8d0b8de3b4dd38ac515654
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-14 19:44:12 +0300

    Add extensions

--- src/Services/Chat/Chat.API/Extensions/DependencyInjection.cs
+++ src/Services/Chat/Chat.API/Extensions/DependencyInjection.cs
@@ -1,10 +1,41 @@
-﻿namespace Сhat.API.Extensions
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions;
+
+namespace Chat.API.Extensions
 {
     public static class DependencyInjection
     {
-        public static void ConfigureAPI(this IServiceCollection services)
+        public static void ConfigureAPI(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddControllers();
+            services.ConfigureVersioning();
+            services.ConfigureCors();
+            services.ConfigureAuthentication(configuration);
+            services.ConfigureAuthorization();
+            services.ConfigureSwagger(configuration);
+
+        }
+
+        private static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
+
+        private static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders("X-Pagination"));
+            });
         }
     }
 }
