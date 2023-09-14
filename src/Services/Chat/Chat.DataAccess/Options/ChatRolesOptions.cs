869d9228a874226faf7b952afe2d2d3edc036e6e
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-14 19:30:25 +0300

    Add chat roles options

---
+++ src/Services/Chat/Chat.DataAccess/Options/ChatRolesOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Chat.DataAccess.Options
+{
+    public class ChatRolesOptions
+    {
+        public string Admin { get; set; } = string.Empty;
+        public int AdminRolePrecedence { get; set; }
+        public string User { get; set; } = string.Empty;
+        public int UserRolePrecedence { get; set; }
+        public string Creator { get; set; } = string.Empty;
+        public int CreatorRolePrecedence { get; set; }
+    }
+}
