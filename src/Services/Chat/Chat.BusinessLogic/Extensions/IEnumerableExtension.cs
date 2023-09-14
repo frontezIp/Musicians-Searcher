743f93abaa6a4dbef4f597517ba8cd22d94e46c2
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-14 19:38:17 +0300

    Add extensions

---
+++ src/Services/Chat/Chat.BusinessLogic/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,21 @@
+﻿using Chat.BusinessLogic.RequestFeatures;
+
+namespace Chat.BusinessLogic.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static MetaData GetMetaData(this IEnumerable<object> enumerable,
+            long count,
+            int pageNumber, 
+            int pageSize) 
+        {
+            return new MetaData
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (long)Math.Ceiling(count / (double)pageSize)
+            };
+        }
+    }
+}
