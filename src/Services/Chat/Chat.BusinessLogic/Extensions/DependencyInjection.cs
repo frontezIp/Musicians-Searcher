90cc27f3726cc4b20d335f0af4e5f7c1fd8c5c4f
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-26 18:18:56 +0300

    Add kafka configurations

--- src/Services/Chat/Chat.BusinessLogic/Extensions/DependencyInjection.cs
+++ src/Services/Chat/Chat.BusinessLogic/Extensions/DependencyInjection.cs
@@ -15,6 +15,9 @@ using MapsterMapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shared.Messages.IdentityMessages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Chat.BusinessLogic.Options;
 
 namespace Chat.BusinessLogic.Extensions
 {
@@ -27,6 +30,8 @@ namespace Chat.BusinessLogic.Extensions
             services.ConfigureServiceHelpers();
             services.ConfigureServices();
             services.ConfigureOptions(configuration);
+            services.ConfigureMessageHandlers();
+            services.ConfigureMessageOutboxConsumers(configuration);
         }
 
         private static void AddMappings(this IServiceCollection services)
@@ -37,12 +42,46 @@ namespace Chat.BusinessLogic.Extensions
             services.AddScoped<IMapper, ServiceMapper>();
         }
 
+        private static void ConfigureMessageHandlers(this IServiceCollection services)
+        {
+            services.AddScoped<IOutboxMessageHandler<string>, OutboxMessageHandler<string>>();
+            services.AddScoped<IConsumerHandler<string, UserCreatedMessage>, UserCreatedMessageHandler>();
+        }
+
+        private static void ConfigureGrpcClient(this IServiceCollection services)
+        {
+            services.AddScoped<ErrorInterceptor>();
+            services.AddScoped<IMusicianClient, MusicianClient>();
+            services.AddGrpcClient<Musician.MusicianClient>((p, o) =>
+            {
+                var grpcConfig = p.GetRequiredService<IOptions<GrpcConfigOptions>>();
+                o.Address = new Uri(grpcConfig.Value.MusicianUrl);
+            }).AddInterceptor<ErrorInterceptor>();
+        }
+        private static void ConfigureMessageOutboxConsumers(this IServiceCollection services, IConfiguration configuration)
+        {
+            var topicOptions = new KafkaTopicOptions();
+            configuration.GetSection(nameof(KafkaTopicOptions)).Bind(topicOptions);
+
+            services.AddHostedService<OutboxMessagesBackgroundConsumer<string>>(provider =>
+            {
+                var logger = provider.GetRequiredService<ILogger<OutboxMessagesBackgroundConsumer<string>>>();
+                var consumerConfig = provider.GetRequiredService<IOptions<ConsumerConfig>>();
+                return new OutboxMessagesBackgroundConsumer<string>(
+                    topicOptions.IdentityTopic,
+                    provider,
+                    consumerConfig,
+                    logger);
+            });
+        }
+
         private static void ConfigureServices(this IServiceCollection services)
         {
             services.AddScoped<IChatParticipantService, ChatParticipantService>();
             services.AddScoped<IChatRoomService, ChatRoomService>();    
             services.AddScoped<IChatRoleService, ChatRoleService>();    
             services.AddScoped<IMessageService, MessageService>();  
+            services.AddScoped<IMessengerUserService, MessengerUserService>();
         }
 
         private static void ConfigureServiceHelpers(this IServiceCollection services)
@@ -54,9 +93,35 @@ namespace Chat.BusinessLogic.Extensions
             services.AddScoped<IChatRoleServiceHelper, ChatRoleServiceHelper>();
         }
 
+        private static void ConfigureConsumers(this IServiceCollection services, IConfiguration configuration)
+        {
+            var topicOptions = new KafkaTopicOptions();
+            configuration.GetSection(nameof(KafkaTopicOptions)).Bind(topicOptions);
+            services.AddKafkaConsumer<string, UserCreatedMessage, UserCreatedMessageHandler>(o =>
+            {
+                o.Topic = topicOptions.IdentityTopic;
+            });
+        }
+
+        private static void AddKafkaConsumer<Tk, Tv, THandler>(this IServiceCollection services,
+            Action<KafkaTopicSelectionOption<Tk, Tv>> configAction)
+            where Tv : class
+            where THandler : class, IConsumerHandler<Tk, Tv>
+        {
+            services.AddScoped<IConsumerHandler<Tk, Tv>, THandler>();
+
+            services.AddHostedService<MessagesBackgroundConsumer<Tk, Tv>>();
+
+            services.Configure(configAction);
+        }
+
         private static void ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<ChatRolesOptions>(configuration.GetSection(nameof(ChatRolesOptions)));
+            services.Configure<ConsumerConfig>(configuration.GetSection(nameof(ConsumerConfig)));
+            services.Configure<KafkaTopicOptions>(configuration.GetSection(nameof(KafkaTopicOptions)));
+            services.Configure<GrpcConfigOptions>(configuration.GetSection(nameof(GrpcConfigOptions))); 
+            services.Configure<HangfireOptions>(configuration.GetSection(nameof(HangfireOptions)));
         }
 
         private static void ConfigureFluentValidators(this IServiceCollection services)
---
+++ src/Services/Chat/Chat.BusinessLogic/Kafka/MessagesAssemblyReference.cs
@@ -0,0 +1,11 @@
+﻿using Shared;
+using System.Reflection;
+
+namespace Chat.BusinessLogic.Kafka
+{
+    public static class MessagesAssemblyReference
+    {
+        public static Assembly Assembly
+            => typeof(SharedAssemblyReference).Assembly;
+    }
+}
