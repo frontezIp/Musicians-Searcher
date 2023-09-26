a9284d3ef83ebd1ac052a6e01b1b9852ad5f1748
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-09-26 18:02:36 +0300

    Add outbox repositories

---
+++ src/Services/Identity/Identity.Infrastructure/MessageBroker/TranscationalOutbox/OutboxMessageRepository.cs
@@ -0,0 +1,38 @@
+﻿using Identity.Infrastructure.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Shared.Messages;
+
+namespace Identity.Infrastructure.MessageBroker.TranscationalOutbox
+{
+    internal class OutboxMessageRepository : IOutboxMessageRepository
+    {
+        private readonly IdentityContext _context;
+
+        public OutboxMessageRepository(IdentityContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(OutboxMessage message)
+        {
+            await _context.AddAsync(message);
+        }
+
+        public void Update(OutboxMessage message)
+        {
+            _context.Update(message);
+        }
+
+        public async Task<IEnumerable<OutboxMessage>> GetOutboxMessagesToSentAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.OutboxMessages
+                .Where(outboxMessage => outboxMessage.Status == Shared.Enums.OutboxMessageStatus.New)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
