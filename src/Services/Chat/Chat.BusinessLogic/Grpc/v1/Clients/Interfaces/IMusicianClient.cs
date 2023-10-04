8d86156251f2a4e044b057e8ac5339eca68577f2
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-10-05 02:11:32 +0300

    Add grpc client definition

---
+++ src/Services/Chat/Chat.BusinessLogic/Grpc/v1/Clients/Interfaces/IMusicianClient.cs
@@ -0,0 +1,10 @@
+﻿using Musicians.Application.Grpc.v1;
+
+namespace Chat.BusinessLogic.Grpc.v1.Clients.Interfaces
+{
+    public interface IMusicianClient
+    {
+        Task<GetMusicianFriendsResponse> GetMessengerUserFriendsAsync(string messengerUserId);
+        Task<GetMusicianInformationResponse> GetMessengerUserProfileAsync(string messengerUserId);
+    }
+}
