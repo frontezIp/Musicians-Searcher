3eb1f406f13d181e5775b57b9cfc24227e49e9db
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-10-05 02:26:43 +0300

    Added client musician profile response dto

---
+++ src/Services/Chat/Chat.BusinessLogic/DTOs/ResponseDTOs/MessengerUserProfileResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Chat.BusinessLogic.DTOs.ResponseDTOs
+{
+    public class MessengerUserProfileResponseDto
+    {
+        public Guid Id { get; set; }    
+        public string? Goal { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public int Age { get; set; }    
+        public string Location = string.Empty;
+        public string? Biography { get; set; }
+        public int SubscribersCount { get; set; }
+        public int FriendsCount { get; set; }   
+    }
+}
