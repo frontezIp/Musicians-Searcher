1c52d596c850e951acf3d65ee24cd78c7cc39eb9
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-10-05 02:27:12 +0300

    Add client api for the grpc related calls

---
+++ src/Services/Chat/Chat.API/Controllers/v1/MessengerUsersController.cs
@@ -0,0 +1,35 @@
+﻿using Chat.BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.API.Controllers.v1
+{
+    public class MessengerUsersController : BaseApiController
+    {
+        private readonly IMessengerUserService _messengerUserService;
+
+        public MessengerUsersController(IMessengerUserService messengerUserService)
+        {
+            _messengerUserService = messengerUserService;
+        }
+
+        [HttpGet("{messengerUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetMessengerUserProfileAsync(Guid messengerUserId)
+        {
+            var messengerToReturn = await _messengerUserService.GetMessengerUserProfileInfoAsync(messengerUserId, HttpContext.RequestAborted);
+
+            return Ok(messengerToReturn);
+        }
+
+        [HttpGet("{messengerUserId}/friends")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetMessengerUserFriendsAsync(Guid messengerUserId)
+        {
+            var messengerUserFriends = await _messengerUserService.GetMessengerUserFriendsAsync(messengerUserId, HttpContext.RequestAborted);
+
+            return Ok(messengerUserFriends);
+        }
+    }
+}
