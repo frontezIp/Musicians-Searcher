165b3b31dd25c990e99d8092af1537366df8bc40
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-10-14 16:10:52 +0300

    Add hangfire message delaying functionality

---
+++ src/Services/Chat/Chat.BusinessLogic/HangfireServices/Interfaces/IHangfireMessageService.cs
@@ -0,0 +1,9 @@
+﻿using Chat.BusinessLogic.DTOs.RequestDTOs.MessageRequestsDTOs;
+
+namespace Chat.BusinessLogic.HangfireServices.Interfaces
+{
+    public interface IHangfireMessageService
+    {
+        Task SendDelayedMessageAsync(Guid messengerUserId, Guid chatRoomId, CreateMessageRequestDto createMessageRequestDto, CancellationToken cancellationToken = default);
+    }
+}
--- src/Services/Chat/Chat.BusinessLogic/Services/Interfaces/IMessageService.cs
+++ src/Services/Chat/Chat.BusinessLogic/Services/Interfaces/IMessageService.cs
@@ -5,7 +5,7 @@ namespace Chat.BusinessLogic.Services.Interfaces
 {
     public interface IMessageService
     {
-        Task CreateMessageAsync(Guid messengerUserId, Guid chatRoomId, CreateMessageRequestDto createMessageRequestDto, CancellationToken cancellationToken = default);
+        Task<MessageResponseDto?> CreateMessageAsync(Guid messengerUserId, Guid chatRoomId, CreateMessageRequestDto createMessageRequestDto, CancellationToken cancellationToken = default);
         Task DeleteMessagesAsync(Guid messengerUserId, Guid chatRoomId, IEnumerable<Guid> messagesToDeleteIds, CancellationToken cancellationToken = default);
         Task UpdateMessageAsync(Guid messengerUserId, Guid chatRoomId, Guid messageId, UpdateMessageRequestDto updateMessageRequestDto, CancellationToken cancellationToken = default);
     }
