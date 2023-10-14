13e083063b1c36969a43620d66e8a10ed7ee1de6
Maxim Padalitsa <padalitsa2055@gmail.com>
2023-10-14 16:13:33 +0300

    Add delay parameter validation

--- src/Services/Chat/Chat.BusinessLogic/Validators/RequestValidators/MessageValidators/CreateMessageRequestDtoValidator.cs
+++ src/Services/Chat/Chat.BusinessLogic/Validators/RequestValidators/MessageValidators/CreateMessageRequestDtoValidator.cs
@@ -13,6 +13,14 @@ namespace Chat.BusinessLogic.Validators.RequestValidators.MessageValidators
                 .WithMessage("Message must not be empty")
                 .MaximumLength(400)
                 .WithMessage("Message must be 400 symbols or fewer");
+
+            When(message => message.Delay != null, () =>
+            {
+                RuleFor(message => message.Delay!.Value)
+                    .NotEmpty()
+                    .GreaterThan(DateTime.Now.AddMinutes(1))
+                    .WithMessage("Delay must be at least one minute ahead over current time");
+            });
         }
     }
 }
